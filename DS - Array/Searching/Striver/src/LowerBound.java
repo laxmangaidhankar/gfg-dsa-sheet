b6fd83cb8b32e461dfe83b3fdb7079b3c27320ea
Laxman Gaidhankar <laxmangaidhankar44@gmail.com>
2026-09-24 15:14:31 +0530

    ST2 - Lower Bound

---
+++ DS - Array/Searching/Striver/src/LowerBound.java
@@ -0,0 +1,43 @@
+public class LowerBound {
+
+    public static void main(String[] args) {
+        int[] nums= {3,5,8,15,19};
+        System.out.println(lowerBoundO(nums, 9));
+    }
+
+
+    static int lowerBoundO(int[] nums, int x) {
+        int n = nums.length;
+
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+
+        }
+
+        return low;
+    }
+
+
+    static int lowerBoundBF(int[] nums, int x) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
